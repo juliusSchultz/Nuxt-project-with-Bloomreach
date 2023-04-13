2860b3dcf9eeacf224a63cd5768b9d80e05bbfe8
julius.schultz <julius.schultz@diva-e.com>
2023-04-13 12:26:19 +0200

    change plugin from .ts to .js

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src:'~/plugins/brxm.ts', mode: 'client' }
+    { src:'~/plugins/brxm.js' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
