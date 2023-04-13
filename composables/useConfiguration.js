7977abdd07af4bf5fcdd0cc2d40170f142a7414c
julius.schultz <julius.schultz@diva-e.com>
2023-04-13 17:31:32 +0200

    integrate environment variables manually

--- composables/useConfiguration.js
+++ composables/useConfiguration.js
@@ -3,8 +3,8 @@ import cookie from 'cookie';
 import { ref } from 'vue';
 
 export const useConfiguration = () => {
-    const accountName = process.env.ACCOUNT_NAME;
-    const channelName = process.env.CHANNEL_NAME;
+    const accountName = 'diva-e-preview.bloomreach.io';
+    const channelName = 'deployed-channel';
     const pathName = '/'
     const pathSearch = '?token=eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI4ZGY5NGFiOS03MzY5LTQyMjYtYWZiOC0wNWIwNjFlZGEyNDkifQ.E16C0lDC_89ts8_-CtxRD1-xkCa8e6EnWNbn6-TNhn3Ep8LhRdclEFxHpr2obliyZHJlP3SIoQUSGqqKnqL87TULOP7MS4MehWNSm-iqq05sjH6IPIg5u5XG-4EQQF2mNE14CahZAZlrfQwZbJbL1Wa5qTS4NqOME7ueVbyTCc4R1WVQ9Ar8UjMzYWZjZFXA5SraI6guIrvJnGTylsBqHFPcPe02PLM-6e8Bmy6nUfpkj3546rniiTsWnKRcFRdbNEMuZ-paMYmqcZOh8MLU31qGTpwzPj0sw4J0MveMRCPOjrCzUwag8pzOfy4t1MCL5-HWh4STuM1b8UMYkpjxlw&server-id=cms-blue-diva-e-preview-one-669448544f-pgdh6&endpoint=https%3A%2F%2Fdiva-e-preview.bloomreach.io%2Fdelivery%2Fsite%2Fv1%2Fchannels%2Fdeployed-channel%2Fpages'
 
